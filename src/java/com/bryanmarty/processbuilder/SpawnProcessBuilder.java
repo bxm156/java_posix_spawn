84cb829161232040a9b1eb11fe8c5fb79dfc4363
Bryan Marty <bxm156@case.edu>
2012-02-18 15:45:05 -0500

    Continue improving and adding functionality to SpawnProcessBuilder.

--- src/java/com/bryanmarty/processbuilder/SpawnProcessBuilder.java
+++ src/java/com/bryanmarty/processbuilder/SpawnProcessBuilder.java
@@ -1,9 +1,13 @@
 package com.bryanmarty.processbuilder;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.annotation.processing.ProcessingEnvironment;
 
 import net.axiak.runtime.SpawnRuntime;
 import net.axiak.runtime.SpawnedProcess;
@@ -13,6 +17,7 @@ public class SpawnProcessBuilder {
 	private boolean redirectErrorStream;
 	private File directory;
 	private LinkedList<String> command;
+	private Map<String, String> enviornment;
 	
 	private static final SpawnRuntime RUNTIME = new SpawnRuntime(Runtime.getRuntime());
 
@@ -21,6 +26,7 @@ public class SpawnProcessBuilder {
 			throw new NullPointerException();
 		}
 		this.command = new LinkedList<String>(command);
+		this.enviornment = System.getenv();
 	}
 	SpawnProcessBuilder(String command) {
 		if(command == null) {
@@ -47,7 +53,11 @@ public class SpawnProcessBuilder {
 	}
 	
 	public Map<String,String> environment() {
-		
+		SecurityManager secMan = System.getSecurityManager();
+		if(secMan != null) {
+			secMan.checkPermission(new RuntimePermission("getenv.*"));
+		}
+		return this.enviornment;
 	}
 	
 	public boolean redirectErrorStream() {
@@ -58,16 +68,35 @@ public class SpawnProcessBuilder {
 		this.redirectErrorStream = redirectErrorStream;
 		return this;
 	}
-	public SpawnedProcess start() {
+	private String[] convertEnvironment(Map<String,String> env) {
+		if(env == null) {
+			//TODO
+		}
+		String[] envp = new String[env.size()];
+		int count = 0;
+		for(Map.Entry<String, String> e : env.entrySet()) {
+			envp[count] = e.getKey() + '=' + e.getValue();
+			count++;
+		}
+		return envp;
+	}
+	
+	public Process start() throws IOException {
 		return start(false);
 	}
-	public SpawnedProcess start(boolean forceStart) {
-		
+	
+	public Process start(boolean forceStart) throws IOException {
 		if(!RUNTIME.isLinuxSpawnLoaded() && !forceStart) {
 			return null;
 		}
 		
+		SecurityManager secMan = System.getSecurityManager();
+		if(secMan != null) {
+			secMan.checkExec(command.getFirst());
+		}
+		
+		String[] commandArray = command.toArray(new String[command.size()]);
+		String[] envp = convertEnvironment(this.enviornment);
+		return RUNTIME.exec(commandArray, envp, this.directory, this.redirectErrorStream);
 	}
-	
-	
 }
